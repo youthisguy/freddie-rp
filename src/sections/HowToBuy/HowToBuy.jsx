13095d663895431c01e4b942a56bedc2567fe592
Sam Steel <samuel.ini145@gmail.com>
2024-12-05 22:54:33 +0100

    TOKENOMICS

--- src/sections/HowToBuy/HowToBuy.jsx
+++ src/sections/HowToBuy/HowToBuy.jsx
@@ -4,7 +4,7 @@ const HowToBuy = () => {
   return (
     <div className="bg-[#275CFE] mt-56 relative">
       <h1 className="pb-32 text-center max-w-prose leading-relaxed sm:leading-relaxed mx-auto hero-h1 text-white text-5xl font-bold sm:text-5xl [text-shadow:_-2px_-2px_0_#000,_2px_-2px_0_#000,_-2px_2px_0_#000,_2px_2px_0_#000]">
-        How To Buy
+        HOW TO BUY
       </h1>
 
 
--- src/sections/Tokenomics/Tokenomics.jsx
+++ src/sections/Tokenomics/Tokenomics.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Tokenomics = () => {
   return (
-    <div className='bg-[#DD1A1B]'>Tokenomics</div>
+    <div className='bg-[#F46023]'>
+    <div className='text-center max-w-prose leading-relaxed sm:leading-relaxed mx-auto hero-h1 text-white text-5xl font-bold sm:text-5xl [text-shadow:_-2px_-2px_0_#000,_2px_-2px_0_#000,_-2px_2px_0_#000,_2px_2px_0_#000]'>TOKENOMICS</div>
+    <div className="pt-6">
+      <img src="./Tokenomics-FDV.png" alt="Tokenomics-FDV" />
+    </div>
+    </div>
   )
 }
 
