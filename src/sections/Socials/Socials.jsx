a760dfd4fe93d0a7c5281120f25031d74e16036c
Sam Steel <samuel.ini145@gmail.com>
2024-12-06 00:37:18 +0100

    speed

--- src/sections/Socials/Socials.jsx
+++ src/sections/Socials/Socials.jsx
@@ -7,6 +7,24 @@ const Socials = () => {
       <div className="pt-36 text-center max-w-prose leading-relaxed sm:leading-relaxed mx-auto hero-h1 text-white text-5xl font-bold sm:text-5xl [text-shadow:_-2px_-2px_0_#000,_2px_-2px_0_#000,_-2px_2px_0_#000,_2px_2px_0_#000]">
         OUR SOCIALS
       </div>
+      <div className="flex justify-center items-center gap-6 mt-8">
+    <a
+      href="https://x.com/FDVonsol"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="transition-transform transform hover:scale-110 hover:rotate-3"
+    >
+      <img src="./twitter.png" alt="Twitter" className="w-16 h-16" />
+    </a>
+    <a
+      href="https://t.me/FDVonsol"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="transition-transform transform hover:scale-110 hover:-rotate-3"
+    >
+      <img src="./tg.png" alt="Telegram" className="w-16 h-16" />
+    </a>
+  </div>
       <img src="./SOCIALS.png" alt="Background" />
     </div>
   );
