f099ad5a150b43d41f1e529d968af077facf8783
Sam Steel <samuel.ini145@gmail.com>
2024-12-06 09:17:05 +0100

    tokenomixs

--- src/sections/Tokenomics/Tokenomics.jsx
+++ src/sections/Tokenomics/Tokenomics.jsx
@@ -3,14 +3,17 @@ import React from "react";
 const Tokenomics = () => {
   return (
     <div className="bg-[#F46023]">
-      <div className="sm:pt-36 pt-11 text-center max-w-prose leading-relaxed sm:leading-relaxed mx-auto hero-h1 text-white text-4xl font-bold sm:text-5xl [text-shadow:_-2px_-2px_0_#000,_2px_-2px_0_#000,_-2px_2px_0_#000,_2px_2px_0_#000]">
+      <div className="sm:pt-36 pt-8 text-center max-w-prose leading-relaxed sm:leading-relaxed mx-auto hero-h1 text-white text-4xl font-bold sm:text-5xl [text-shadow:_-2px_-2px_0_#000,_2px_-2px_0_#000,_-2px_2px_0_#000,_2px_2px_0_#000]">
         TOKENOMICS
       </div>
       <div className="pt-6 relative">
-        <img src="./Tokenomics-FDV.png" alt="Tokenomics-FDV" />
-
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-center sm:-mr-60 -mr-9">
-          <p className="chillax-text text-[#E45421] pb-5 sm:text-2xl break-all text-sm sm:w-full  text-wrap sm:-mt-44 -mt-2">
+        <img
+          src="./Tokenomics-FDV.png"
+          alt="Tokenomics-FDV"
+          class="w-[500px] h-[200px] sm:w-[500px] sm:h-[350px] md:w-auto md:h-auto mx-auto object-cover transition-transform duration-300 ease-in-out"
+        />
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center sm:-mr-60 ml-16">
+          <p className="chillax-text text-[#E45421] pb-5 sm:text-2xl break-all text-sm max-w-fit  text-wrap sm:-mt-44 -mt-2">
             TOKEN ADDRESS
           </p>
           <p className="chillax-text text-[#000] text-xs sm:text-base sm:max-full break-all max-w-[50%]">
