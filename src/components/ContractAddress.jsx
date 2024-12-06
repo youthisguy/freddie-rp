c01e7946946ddc5e3690923d39de3fcb0c242127
Sam Steel <samuel.ini145@gmail.com>
2024-12-06 03:00:44 +0100

    updates

--- src/components/ContractAddress.jsx
+++ src/components/ContractAddress.jsx
@@ -4,8 +4,8 @@ const ContractAddress = () => {
   return (
     <>
       <div className="pt-6">
-        <img src="./hero.png" alt="hero" />
-      </div>
+        <img src="./hero.png" alt="hero" className="mt-16"/>
+      </div> 
       <div className="bg-[#EEB678] -mt-4 flex items-center flex-col justify-center">
         <button className="rounded-[30px] mt-8 border-2 text-black bg-white border-black px-4 sm:px-6 py-2 sm:py-3 shadow-md hover:bg-gray-100 transition text-sm sm:text-base md:text-xl break-all max-w-[90%] sm:max-w-none cursor-copy">
           CA: DAMpC91BA8JnaXCVTkACFb4js4dkLfrKr16e53fmpump
