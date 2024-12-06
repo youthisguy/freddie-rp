c5826d127c16afec7b57e6b6f3e27ca4fc10596c
Sam Steel <samuel.ini145@gmail.com>
2024-12-06 17:03:57 +0100

    small change

--- src/sections/Backstory/BackStory.jsx
+++ src/sections/Backstory/BackStory.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BackStory = () => {
   return (
     <div className="bg-[#EEB678] relative sm:pb-20 pb-0">
-      <img src="./CA-img.png" alt="CA-img" className="w-full h-1/3 -mt-2" />
+      <img src="./CA-img.png" alt="CA-img" className="w-full h-1/3 -mt-2  transform -translate-y-0.5" />
       <img
         src="./Backstory-2.png"
         alt="backstory"
