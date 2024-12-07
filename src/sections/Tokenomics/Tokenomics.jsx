ad7603031af6295b899be2444153b1ae3c58c1fa
Sam Steel <samuel.ini145@gmail.com>
2024-12-07 08:44:37 +0100

    save updates

--- src/sections/Tokenomics/Tokenomics.jsx
+++ src/sections/Tokenomics/Tokenomics.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Tokenomics = () => {
   return (
-    <div className="bg-[#F46023] -mt-[35px]">
+    <div className="bg-[#F46023] -mt-[35px] sm:-mt-[60px]">
       <div className="sm:pt-36 pt-16 text-center max-w-prose leading-relaxed sm:leading-relaxed mx-auto hero-h1 text-white text-4xl font-bold sm:text-5xl [text-shadow:_-2px_-2px_0_#000,_2px_-2px_0_#000,_-2px_2px_0_#000,_2px_2px_0_#000] z-10">
         TOKENOMICS
       </div>
